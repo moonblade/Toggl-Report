a44ebe2cc8fa6cf47c987243abd9cb6997f1491e
mnishamk <mnishamk@commvault.com>
2023-06-05 19:54:16 +0530

    start with report

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 import os
+import json
+from datetime import datetime, timedelta
 
 base = os.path.dirname(__file__)
 secret= base + "/secret"
@@ -9,3 +11,15 @@ with open(apiKeyFile) as f:
 
 with open(secret + "/workspacename") as f:
     workspacename = f.read().strip()
+
+with open(secret + "/clients.json") as f:
+    clients = json.load(f);
+
+if int(datetime.utcnow().strftime("%d")) > 15:
+    endDate = datetime.utcnow().strftime("%Y-%m-%d")
+    startDate = datetime.utcnow().replace(day=1).strftime("%Y-%m-%d")
+else:
+    endDate = (datetime.utcnow().replace(day=1) - timedelta(days=1)).strftime("%Y-%m-%d")
+    startDate = (datetime.utcnow().replace(day=1) - timedelta(days=1)).replace(day=1).strftime("%Y-%m-%d")
+
+reportFilePath = base + "/report.json"
